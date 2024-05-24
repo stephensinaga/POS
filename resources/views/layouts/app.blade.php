3dec96c7f3095922ff46e4bc99cbd26aacc8991e
stefansinaga <stefansinaga10@gmail.com>
2024-05-24 17:39:31 +0700

    perbaiki tampilanlog dan reg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,8 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Dashboard - NiceAdmin Bootstrap Template</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-
-  <!-- Favicons -->
-  <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
-  <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+  <title>POS</title>
+  <link rel="icon" href="{{ asset('assets/img/company_logo.png')}}" type="image/png"> 
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
@@ -28,14 +23,6 @@
 
   <!-- Template Main CSS File -->
   <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: NiceAdmin
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Updated: Apr 20 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,15 +3,15 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
     <li class="nav-item">
-        <a class="nav-link " href="index.html">
+        <a class="nav-link " href="{{ route('dashboard')}}">
         <i class="bi bi-grid"></i>
         <span>Dashboard</span>
         </a>
     </li>
 
     <li class="nav-item">
-        <a class="nav-link " href="">
-        <i class="bi bi-grid"></i>
+        <a class="nav-link " href="#">
+        <i class="bi bi-box-fill"></i>
         <span>Barang</span>
         </a>
     </li>
