e8cad5eba3e921a7a44497479e2ba80dd88c12bb
stefansinaga <stefansinaga10@gmail.com>
2024-05-31 16:50:52 +0700

    perbaiki Struktur File

--- database/migrations/2024_05_31_073442_create_products_table.php
+++ database/migrations/2024_05_31_073442_create_products_table.php
@@ -11,14 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('transactions', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('user_id');
-            $table->string('ctgr_id');
-            $table->string('amount');
-            $table->string('type');
-            $table->string('trsc_desc');
-            $table->string('transaction_date');
             $table->timestamps();
         });
     }
@@ -28,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('products');
     }
 };
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -32,68 +32,6 @@
         </a><!-- End Notification Icon -->
 
         <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
-            <li class="dropdown-header">
-            You have 4 new notifications
-            <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-            </li>
-            <li>
-            <hr class="dropdown-divider">
-            </li>
-
-            <li class="notification-item">
-            <i class="bi bi-exclamation-circle text-warning"></i>
-            <div>
-                <h4>Lorem Ipsum</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>30 min. ago</p>
-            </div>
-            </li>
-
-            <li>
-            <hr class="dropdown-divider">
-            </li>
-
-            <li class="notification-item">
-            <i class="bi bi-x-circle text-danger"></i>
-            <div>
-                <h4>Atque rerum nesciunt</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>1 hr. ago</p>
-            </div>
-            </li>
-
-            <li>
-            <hr class="dropdown-divider">
-            </li>
-
-            <li class="notification-item">
-            <i class="bi bi-check-circle text-success"></i>
-            <div>
-                <h4>Sit rerum fuga</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>2 hrs. ago</p>
-            </div>
-            </li>
-
-            <li>
-            <hr class="dropdown-divider">
-            </li>
-
-            <li class="notification-item">
-            <i class="bi bi-info-circle text-primary"></i>
-            <div>
-                <h4>Dicta reprehenderit</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>4 hrs. ago</p>
-            </div>
-            </li>
-
-            <li>
-            <hr class="dropdown-divider">
-            </li>
-            <li class="dropdown-footer">
-            <a href="#">Show all notifications</a>
-            </li>
 
         </ul><!-- End Notification Dropdown Items -->
 
@@ -101,65 +39,12 @@
 
         <li class="nav-item dropdown">
 
-        <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-            <i class="bi bi-chat-left-text"></i>
-            <span class="badge bg-success badge-number">3</span>
-        </a><!-- End Messages Icon -->
+            <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                <i class="bi bi-chat-left-text"></i>
+                <span class="badge bg-success badge-number">3</span>
+            </a><!-- End Messages Icon -->
 
         <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
-            <li class="dropdown-header">
-            You have 3 new messages
-            <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-            </li>
-            <li>
-            <hr class="dropdown-divider">
-            </li>
-
-            <li class="message-item">
-            <a href="#">
-                <img src="assets/img/messages-1.jpg" alt="" class="rounded-circle">
-                <div>
-                <h4>Maria Hudson</h4>
-                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                <p>4 hrs. ago</p>
-                </div>
-            </a>
-            </li>
-            <li>
-            <hr class="dropdown-divider">
-            </li>
-
-            <li class="message-item">
-            <a href="#">
-                <img src="assets/img/messages-2.jpg" alt="" class="rounded-circle">
-                <div>
-                <h4>Anna Nelson</h4>
-                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                <p>6 hrs. ago</p>
-                </div>
-            </a>
-            </li>
-            <li>
-            <hr class="dropdown-divider">
-            </li>
-
-            <li class="message-item">
-            <a href="#">
-                <img src="assets/img/messages-3.jpg" alt="" class="rounded-circle">
-                <div>
-                <h4>David Muldon</h4>
-                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                <p>8 hrs. ago</p>
-                </div>
-            </a>
-            </li>
-            <li>
-            <hr class="dropdown-divider">
-            </li>
-
-            <li class="dropdown-footer">
-            <a href="#">Show all messages</a>
-            </li>
 
         </ul><!-- End Messages Dropdown Items -->
 
@@ -168,14 +53,18 @@
         <li class="nav-item dropdown pe-3">
 
         <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+            <span class="d-none d-md-block dropdown-toggle ps-2">
+            <small>{{ auth()->user()->nama }}</small>
+                <br>
+                <small>{{ auth()->user()->level }}</small>
+            </span>
         </a><!-- End Profile Iamge Icon -->
 
         <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-            <h6>Kevin Anderson</h6>
-            <span>Web Designer</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+					<small>{{ auth()->user()->level }}</small>
+				</span>
             </li>
             <li>
             <hr class="dropdown-divider">
